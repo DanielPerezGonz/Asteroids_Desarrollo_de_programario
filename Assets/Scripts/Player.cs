4ae7c1dc68596286c071895d5dbefbbfdbd7bd28
PC-Enti <daniel.perez@enti.cat>
2022-11-29 10:29:53 +0100

    Asteroids con movimiento, direccion y teletransporte

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed_movement;
+    public float speed_rotation;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        horizontal *= speed_rotation * Time.deltaTime;
+        transform.eulerAngles += new Vector3(0, 0, horizontal);
+
+
+        float vertical = Input.GetAxis("Vertical");
+        if (vertical > 0) { 
+            rb.AddForce(transform.up * vertical * speed_movement * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public float limit_x;
+    public float limit_y;
+    void Update()
+    {
+        //y 5.5 x 9.5
+        if(transform.position.y > limit_y)
+        {
+            transform.position = new Vector3(transform.position.x, -limit_y, transform.position.z);
+        }
+        if(transform.position.y < -limit_y)
+        {
+            transform.position = new Vector3(transform.position.x, limit_y, transform.position.z);
+        }
+        if (transform.position.x > limit_x)
+        {
+            transform.position = new Vector3(-limit_x, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -limit_x)
+        {
+            transform.position = new Vector3(limit_x, transform.position.y, transform.position.z);
+        }
+    }
+}
